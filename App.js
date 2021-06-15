87f127a0185f376e21ef7ccb9f092707d1da205b
nasty23-star <71438930+nasty23-star@users.noreply.github.com>
2021-06-15 20:56:50 +0300

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,13 @@
 import './App.css';
 import Name from './Name'
 import HoverToad from './HoverToad';
-import Quiz from './quiz-futurama'
+import QuizFuturama from './QuizFuturama'
 function App() {
   return (
     <div style={{ backgroundColor: "rgb(78, 140, 190)" }}>
+      <HoverToad />
       <Name />
-     
-     <Quiz />
+      <QuizFuturama />
     </div>
   );
 }
